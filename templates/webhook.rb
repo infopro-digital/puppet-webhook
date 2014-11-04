0fba1066e58051b3f559142f2de4b0d551da78fa
Farzad FARID <ffarid@etai.fr>
2014-11-04 18:43:14 +0100

    Refactor variable & method names.

--- templates/webhook.rb
+++ templates/webhook.rb
@@ -13,29 +13,29 @@ post '/payload' do
   repo_ref    = push['ref']
   ref_array   = repo_ref.split("/")
   ref_type    = ref_array[1]
-  branchName  = ref_array[2]
+  branch_name  = ref_array[2]
   logger.info("repo name = #{repo_name}")
   logger.info("repo ref = #{repo_ref}")
-  logger.info("branch = #{branchName}")
+  logger.info("branch = #{branch_name}")
 
   # Check if the repository contains Puppetfile or hieradata
   if repo_name == repo_puppetfile || repo_name == repo_hieradata
-    logger.info("Deploy r10k for this environment #{branchName}")
-    deployEnv(branchName)
+    logger.info("Deploy r10k for this environment #{branch_name}")
+    deploy_env(branch_name)
   else
     logger.info("Deploy puppet module #{repo_name}")
-    logger.info("Running for branch #{branchName}")
-    deployModule(repo_name)
+    logger.info("Running for branch #{branch_name}")
+    deploy_module(repo_name)
   end
 end
 
 # Some defines.
-def deployEnv(branchname)
-  deployCmd = "/usr/bin/r10k deploy environment #{branchname} -pv"
-  `#{deployCmd}`
+def deploy_env(branchname)
+  deploy_cmd = "/usr/bin/r10k deploy environment #{branchname} -pv"
+  `#{deploy_cmd}`
 end
 
-def deployModule(modulename)
-  deployCmd = "/usr/bin/r10k deploy module #{modulename} -v"
-  `#{deployCmd}`
+def deploy_module(modulename)
+  deploy_cmd = "/usr/bin/r10k deploy module #{modulename} -v"
+  `#{deploy_cmd}`
 end
